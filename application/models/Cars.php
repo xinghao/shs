336d85877608cd90a48e24fb0b56c4d686995cd9
xinghao <Xinghao@airarena.net>
2009-11-18 20:13:40 +1100

    commit

---
+++ application/models/Cars.php
@@ -0,0 +1,226 @@
+<?php
+class Cars extends Business
+{
+	
+	protected $_busTypeId = 9;
+	
+	protected $_cat1Name = 'TYPE';
+	
+	protected $_cat2Name = 'Make';
+
+	protected $_cat3Name = 'Model';
+	
+	
+	
+	
+	public function getBusinessType()
+	{
+		return 'Cars';
+	}
+	
+	public function getCat1Name(){
+		return $this->_cat1Name;
+	}
+
+	
+	protected function _getCat2($cat1 = null)
+	{
+		if (empty($this->_cat2))
+		{
+			$refcat2 = new Refcategory();
+			return $this->_cat2 = $refcat2->getAllCat2OfSpecificCategory($this->_busTypeId);
+		}
+		else
+		{
+			return $this->_cat2; 
+		}
+		
+	}
+
+	protected function _getCat3($cat2 = null)
+	{
+		$refcat3 = new Refcategory();
+		$select = $refcat3->select();
+		
+		if ($cat2 == 'Any' || $cat2 == 'All')
+		{
+			$select->where('busPostTypeId = ? and SubPrimCatId is not null', $this->_busTypeId);
+		}
+		else
+		{
+			$select->where('SubPrimCatId = (select CONCAT( PrimID, SetID)  from ref_category_en where id = ?)', $cat2);
+		}
+		
+		logfire('car cat3 ', $select);
+		return $this->_cat3 = $refcat3->fetchAll($select);
+		
+	}
+	
+	
+	
+	public function search($location, $limit, $offset = 0, $query = null, $cat1 = null, $cat2 = null, $cat3 = null, $cat4 = null, $cat5 = null, $addtionalData = null)
+	{
+		
+		if ($cat1 == 22)
+		{
+			$cat1 = null;	
+		}
+		
+		logfire('realstatesearch cat2', $cat2);
+		if (empty($cat2) || $cat2 = 'Any' || $cat2 = 'ALL')
+		{
+			$cat2 = null;
+		}
+		if (empty($cat3) || $cat3 = 'Any' || $cat3 = 'ALL')
+		{
+			$cat3 = null;	
+		}
+		
+		$select = parent::search($location, $limit, $offset, $query, $cat1, $cat2, $cat3, $cat4, $cat5, $addtionalData);
+
+		// TODO price;
+		foreach($addtionalData as $key=>$value)
+		{
+			logfire($key,$value);	
+		}
+		if (!empty($addtionalData['min']) && $addtionalData['min'] != 'Any' && $addtionalData['min'] != 'ALL')
+		{
+			$select->where('a.priceCalculate >= ?', $addtionalData['min']);
+		}	
+		if (!empty($addtionalData['max']) && $addtionalData['max'] != 'Any' && $addtionalData['max'] != 'ALL')
+		{
+			$select->where('a.priceCalculate <= ?', $addtionalData['max']);
+		}
+		
+		if (empty($cat1))
+		{
+			$keys = '';
+			foreach($this->getCat1Array() as $key=>$value)
+			{
+				if (empty($keys))
+				{
+					$keys = $key;
+				}
+				else
+				{
+					$keys .= ','.$key;
+				}
+			}
+			$select->where('cat1 in ('.$keys.')');
+		}
+		
+		
+		//echo $select;
+		return $select;
+		
+	}
+	
+/*	
+	protected function extraJoin($select)
+	{
+		$psthomes = new Psthome();
+		$select->joinLeft(array('i' => $psthomes->getTableName()),
+		    					'a.id = i.id',
+		    					array('bed' => 'rooms', 'cars'=>'parking', 'bath'=>'baths'));
+		return $select;
+	}
+	*/
+/*
+	public function getResultTableHeader($location = null)
+	{
+		echo '<div class="resultheader" id="realesate">';
+			echo '<span class="date">';
+				echo 'Data';			
+			echo '</span>';
+			echo '<span class="photo">';
+				echo 'Photo';	
+			echo '</span>';
+			echo '<span class="title">';
+				echo 'Title';	
+			echo '</span>';			
+			echo '<span class="property">';
+				echo 'Property Type';			
+			echo '</span>';
+			echo '<span class="bed">';
+				echo 'Bed';			
+			echo '</span>';
+			echo '<span class="cars">';
+				echo 'Cars';
+			echo '</span>';	
+			echo '<span class="bath">';
+				echo 'Bath';			
+			echo '</span>';
+			echo '<span class="price">';
+				echo 'Price ('. iconv("Windows-1252", "UTF-8", $location->getCurrencySymbol()) .')';			
+			echo '</span>';										
+		echo '</div>';
+
+		return parent::getResultTableHeader();
+	}
+*/
+
+
+	public function getResultTable($posting, $location = null)
+	{
+		try{
+		echo '<table class="resultheader" id="realesate" cellspacing=0>';
+			echo '<tr>';
+				echo '<th class="type">';
+					echo 'Type';			
+				echo '</th>';			
+				echo '<th class="make">';
+					echo 'Make';			
+				echo '</th>';
+				echo '<th class="model">';
+					echo 'Model';			
+				echo '</th>';	
+				echo '<th class="title">';
+					echo 'Title';	
+				echo '</th>';
+
+				echo '<th class="cars">';
+					echo 'Cars';			
+				echo '</th>';
+				echo '<th class="bath">';
+					echo 'Bath';			
+				echo '</th>';
+				echo '<th class="photo">';
+					echo 'Photo';	
+				echo '</th>';					
+				echo '<th class="price">';
+					echo 'Price <br />('. $location->getCurrencyAndSymbol() .')';			
+				echo '</th>';	
+			echo '</tr>';
+		foreach($posting as $key=>$value)
+		{
+			echo '<tr class="postingrow">';		
+				echo '<td>';
+					echo $value->cat1name;			
+				echo '</td>';
+				echo '<td>';
+					echo $value->cat2name;			
+				echo '</td>';
+				echo '<td>';
+					echo $value->cat3name;			
+				echo '</td>';
+				echo '<td>';
+					echo $value->title;	
+				echo '</td>';							
+				echo '<td>';
+					echo $value->priceDisplay;			
+				echo '</td>';								
+			echo '</tr>';
+		}
+	
+		echo '</table>';
+		}catch(Exception $e)
+		{
+			logError('get results for realestate', $e);
+			echo $e;
+		}		
+		return parent::getResultTable($posting);
+	}
+		
+
+}    
+?>
---
+++ application/models/forms/CarsRefineForm.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * RefineForm Form
+ *      @version	$Id: ListingForm.php 5195 2009-05-26 07:10:02Z xinghao $
+ *      @package	Application
+ *      @subpackage	admin
+ *      @author		Xinghao <xinghao@airarena.net>
+ *      @copyright	Copyright (c) 2009 Creagency (www.creagency.com.au)
+ */
+class CarsRefineForm extends RefineForm
+{
+	
+
+	
+	protected function buildCat1Element()
+	{
+		parent::buildCat1Element();
+		$this->cat1->setValue(22);
+		$this->addElement($this->cat1);
+	}
+	
+	protected function buildCat2Element()
+	{
+    	$cat2 = new Zend_Form_Element_Select('cat2');
+       	$cat2->setLabel($this->_business->getCat2Name());
+       	$cat2->addMultiOptions($this->_business->getCat2Array(null, true));
+       	$cat2->setValue($this->_business->getFirstCat2(null,true));
+       	$cat2->setAttrib('class','refineformselect');
+        $cat2->setAttrib('onclick',"changeCat2(this.id,this.value, '/ajax/realestate/changecat2/')");
+       	$this->addElement($cat2);
+		
+	}
+	
+	protected function buildCat3Element()
+	{
+       	$cat3 = new Zend_Form_Element_Select('cat3');
+       	$cat3->setLabel($this->_business->getCat3Name());
+       	$cat3->setAttrib('class','refineformselect');
+       	$cat2selected = $this->cat2->getValue();
+
+		
+       	$cat3->addMultiOptions($this->_business->getCat3Array($cat2selected, true));
+        $this->addElement($cat3);
+		
+	}
+
+	protected function  buildExtraElements()
+	{
+		$pricesTable = new Refprice();
+		
+		logfire('$this->_business->getBusinessTypeId()', $this->_business->getBusinessTypeId());
+		$prices = $pricesTable->getPriceOptions($this->_business->getBusinessTypeId(), $this->_location->getSearchRules()->currency);
+		
+       	$min = new Zend_Form_Element_Select('min');
+       	$min->setLabel('Min');
+       	$min->setAttrib('class','refineformmoneyselect');
+       	$min->addMultiOptions($prices);
+       	$this->addElement($min);
+       	
+       	$max = new Zend_Form_Element_Select('max');
+       	$max->setLabel('Max');
+       	$max->setAttrib('class','refineformmoneyselect');
+       	$max->addMultiOptions($prices);
+       	$this->addElement($max);   
+
+       	       	
+		return parent::buildExtraElements();
+	}
+	
+	
+	
+	
+	
+	public function printExtraElements()
+	{
+		echo '<div class="priceselectdiv">';
+		echo '<div class="priceselectdivtitle">Select Price Range: (' . $this->_location->getCurrencyAndSymbol() . ')</div>';
+		echo '<div class="virticalgap"></div>';
+		echo '<div class="priceselectdivbox">';
+		echo '<span class="min">';
+		/*
+		$element = $this->min;
+		echo 'min: ' . $element->getView()->{$element->helper}(
+            $element->getName(),
+            $element->getValue(),
+            $element->getAttribs(),
+            $element->options
+        );
+        */
+		echo $this->min;
+        echo '</span><span class="max">';
+		/*$element = $this->max;
+		echo 'max: ' . $element->getView()->{$element->helper}(
+            $element->getName(),
+            $element->getValue(),
+            $element->getAttribs(),
+            $element->options
+        );
+        */
+        echo $this->max; 
+        echo '</span>';
+        echo '<div class="clear"></div>';
+        echo '</div>';
+        echo '</div>';
+        
+ 		
+		return parent::printExtraElements();
+	}
+	
+	
+	/*
+	public function printLocationElements()
+	{
+		echo 
+		
+		echo $this->city;
+	    echo $this->state;
+		echo $this->country;
+		echo $this->region;
+
+	}
+
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see library/Zend/Zend_Form#populate()
+	 */
+
+	public function populate(array $values)
+	{
+				
+		if(array_key_exists('cat2',$values) && !empty($values['cat2']))
+		{
+			$this->cat3->setMultiOptions($this->_business->getCat3Array($values['cat2'],false));
+		}
+			
+
+
+		
+		// Call the father's render function.
+		return parent::populate($values);
+	}
+/*
+	protected function _getSearchHint($category, $cat1 = null, $cat2 = null, $cat3 = null, $cat4 = null, $Cat5 = null)
+	{
+		if (empty($cat2))
+		{
+			return  $category;
+		}  
+		
+		if ($cat2 == '630')
+		{
+			$cat2Name = 'For SALE';
+		}
+		else
+		{
+			$cat2Name = 'For ' . $this->_business->getCat2NameById($cat2);
+		}
+		
+		if ($cat3 == '631')
+		{
+			return 'ALL ' . $cat2Name;
+		}
+		else
+		{
+			return  $this->_business->getCat3NameById($cat3) . ' ' . $cat2Name;	
+		}		
+	}
+	*/	
+}
