84c5764447c00c21e85161ee459b058eb603878d
xinghao <Xinghao@airarena.net>
2010-02-16 22:27:25 +1100

    commit

--- application/models/tables/Psttitle.php
+++ application/models/tables/Psttitle.php
@@ -5,11 +5,24 @@ class Psttitle extends Zend_Db_Table
 {
 	protected $_name = 'pst_title';
     protected $_primary = 'id';
-    
+
     public  function getTableName()
     {
     	return $this->_name;
     }
-     
+
+    public function getPsttitle($id)
+    {
+    	try{
+			$select = $this->select();
+			$select->where('id = ?', $id);
+			$posting_title = $this->fetchRow($select);
+			return $posting_title->title;
+		}catch(Exception $e)
+		{
+ 			logError('Pst_title failed!', $e);
+ 			throw $e;
+		}
+    }
 }
 
