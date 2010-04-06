bce20a4191706293a3ac73ef2ce91c6878c65702
xinghao <Xinghao@airarena.net>
2010-04-06 23:29:01 +1000

    add static pages

--- includes/frontend_footer.php
+++ includes/frontend_footer.php
@@ -1,13 +1,13 @@
 			    <div id="footer">
 					<ul class="bottomLinks">
-					<li class="first"><a href="#">Advertise&nbsp;With&nbsp;Us</a></li>
-					<li><a href="about Sydney">About Sydney</a></li>
-					<li><a href="#">FAQ</a></li>
-					<li><a href="Contact US">Contact Us</a></li>
+					<li class="first"><a href="advertise with us">Advertise&nbsp;With&nbsp;Us</a></li>
+					<li><a href="about sydney">About Sydney</a></li>
+					<li><a href="faq">FAQ</a></li>
+					<li><a href="contact us">Contact Us</a></li>
 
 					</ul>
 					<ul class="bottomLinks">
-					<li class=""><a href="SydneyHotspot_Privacy_Policy">Privacy Policy</a></li>
-					<li><a href="#">Terms of Service</a></li>
+					<li class=""><a href="sydneyHotspot privacy policy">Privacy Policy</a></li>
+					<li><a href="terms of service">Terms of Service</a></li>
 					</ul>
 		        </div>
